94ee1dcf86622d07ae8e91da913c5cf85f0180d7
Carles Amigó <carles.amigo@socialpoint.es>
2014-11-03 18:36:34 +0100

    adding resource contact

---
+++ manifests/resource/contact.pp
@@ -0,0 +1,89 @@
+define nagios::resource::contact (
+  $ensure = 'present',
+  $exported = false,
+  $manage_htpasswd = $::nagios::params::manage_htpasswd,
+  $htpasswd_file = $::nagios::params::htpasswd_file,
+  $cryptpasswd =  undef,
+  $address1 = undef,
+  $address2 = undef,
+  $address3 = undef,
+  $address4 = undef,
+  $address5 = undef,
+  $address6 = undef,
+  $alias = undef,
+  $can_submit_commands = undef,
+  $contactgroups = undef,
+  $email = undef,
+  $group = undef,
+  $host_notification_commands = undef,
+  $host_notification_options = undef,
+  $host_notification_period = undef,
+  $host_notifications_enabled = undef,
+  $mode = undef,
+  $owner = undef,
+  $pager = undef,
+  $provider = undef,
+  $register = undef,
+  $retain_nonstatus_information = undef,
+  $retain_status_information = undef,
+  $service_notification_commands = undef,
+  $service_notification_options = undef,
+  $service_notification_period = undef,
+  $service_notifications_enabled = undef,
+  $use = undef,
+) {
+
+  $resource_hash = {
+    "${name}"                         => {
+      'ensure'                        => $ensure,
+      'address1'                      => $address1,
+      'address2'                      => $address2,
+      'address3'                      => $address3,
+      'address4'                      => $address4,
+      'address5'                      => $address5,
+      'address6'                      => $address6,
+      'alias'                         => $alias,
+      'can_submit_commands'           => $can_submit_commands,
+      'contactgroups'                 => $contactgroups,
+      'email'                         => $email,
+      'group'                         => $group,
+      'host_notification_commands'    => $host_notification_commands,
+      'host_notification_options'     => $host_notification_options,
+      'host_notification_period'      => $host_notification_period,
+      'host_notifications_enabled'    => $host_notifications_enabled,
+      'mode'                          => $mode,
+      'owner'                         => $owner,
+      'pager'                         => $pager,
+      'provider'                      => $provider,
+      'register'                      => $register,
+      'retain_nonstatus_information'  => $retain_nonstatus_information,
+      'retain_status_information'     => $retain_status_information,
+      'service_notification_commands' => $service_notification_commands,
+      'service_notification_options'  => $service_notification_options,
+      'service_notification_period'   => $service_notification_period,
+      'service_notifications_enabled' => $service_notifications_enabled,
+      'use'                           => $use,
+    }
+  }
+
+  if $exported {
+    @@nagios::resource { $name:
+      type          => 'nagios_contact',
+      resource_hash => $resource_hash,
+    }
+  } else {
+    nagios::resource { $name:
+      type          => 'nagios_contact',
+      resource_hash => $resource_hash,
+    }
+  }
+
+  if $manage_htpasswd {
+    htpasswd { $name:
+      ensure      => $ensure,
+      cryptpasswd => $cryptpasswd,
+      target      => $htpasswd_file,
+    }
+  }
+
+}
