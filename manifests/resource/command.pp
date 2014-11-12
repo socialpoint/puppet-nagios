f406089b2384c0543768d5851e3cb8b5000afd57
Carles Amigó <carles.amigo@socialpoint.es>
2014-11-12 16:42:33 +0100

    adding new command definition

---
+++ manifests/resource/command.pp
@@ -0,0 +1,23 @@
+define puppet-nagios::resource::command (
+  $ensure = 'present',
+  $command_line = undef,
+  $poller_tag = undef,
+  $target = undef,
+  $use = undef,
+) {
+
+  $resource_hash = {
+    "${name}"        => {
+      'ensure'       => $ensure,
+      'command_line' => $command_line,
+      'poller_tag'   => $poller_tag,
+      'use'          => $use,
+    }
+  }
+
+  nagios::resource { $name:
+    type          => 'nagios_command',
+    resource_hash => $resource_hash,
+  }
+
+}
