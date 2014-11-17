6021b0823627211c6e989babd46204e7ab5391d5
Carles Amigó <carles.amigo@socialpoint.es>
2014-11-17 17:18:44 +0100

    adding hostgroup definition

---
+++ manifests/resource/hostgroup.pp
@@ -0,0 +1,35 @@
+define puppet-nagios::resource::hostgroup (
+  $ensure = 'present',
+
+  $action_url = undef,
+  $hostgroup_alias = undef,
+  $hostgroup_members = undef,
+  $members = undef,
+  $notes = undef,
+  $notes_url = undef,
+  $realm = undef,
+  $register = undef,
+  $use = undef,
+) {
+
+  $resource_hash = {
+    "${name}"             => {
+      'ensure'            => $ensure,
+      'action_url'        => $action_url,
+      'alias'             => $hostgroup_alias,
+      'hostgroup_members' => $hostgroup_members,
+      'members'           => $members,
+      'notes'             => $notes,
+      'notes_url'         => $notes_url,
+      'realm'             => $realm,
+      'register'          => $register,
+      'use'               => $use,
+    }
+  }
+
+  nagios::resource { "hostgroup-${name}":
+    type          => 'nagios_hostgroup',
+    resource_hash => $resource_hash,
+  }
+
+}
